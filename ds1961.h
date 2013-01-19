beb09f4511d0fef3ee2bacce2026d5e1e21e4403
Bertrik Sikken <bertrik@sikken.nl>
2013-01-19 02:08:05 +0100

    Add ds1961.h

---
+++ ds1961.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "OneWire.h"
+
+bool ReadAuthWithChallenge(OneWire *ow, const uint8_t id[], int addr, const uint8_t challenge[], uint8_t data[], uint8_t mac[]);
+bool DS1961WriteSecret(OneWire *ow, const uint8_t id[], const uint8_t secret[]);
+
